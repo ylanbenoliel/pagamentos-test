f250fc4196a2793d59470236e0c7ac37077591af
Ylan Benoliel <ylanbenoliel@gmail.com>
2024-11-30 00:11:50 -0300

    Criado model pagamento

---
+++ src/main/java/br/com/ylanbenoliel/pagamentos/model/Pagamento.java
@@ -0,0 +1,133 @@
+package br.com.ylanbenoliel.pagamentos.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "pagamentos")
+public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Positive
+    private BigDecimal valor;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 19)
+    private String numero;
+
+    @NotBlank
+    @Size(max = 7)
+    private String expiracao;
+
+    @NotBlank
+    @Size(min=3, max = 3)
+    private String codigo;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @NotNull
+    private Long pedidoId;
+
+    @NotNull
+    private Long formaDePagamentoId;
+
+    public Pagamento() {
+    }
+
+    public Pagamento(Long id, BigDecimal valor, String nome, String numero, String expiracao, String codigo, Status status, Long pedidoId, Long formaDePagamentoId) {
+        this.id = id;
+        this.valor = valor;
+        this.nome = nome;
+        this.numero = numero;
+        this.expiracao = expiracao;
+        this.codigo = codigo;
+        this.status = status;
+        this.pedidoId = pedidoId;
+        this.formaDePagamentoId = formaDePagamentoId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getExpiracao() {
+        return expiracao;
+    }
+
+    public void setExpiracao(String expiracao) {
+        this.expiracao = expiracao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Long getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(Long pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public Long getFormaDePagamentoId() {
+        return formaDePagamentoId;
+    }
+
+    public void setFormaDePagamentoId(Long formaDePagamentoId) {
+        this.formaDePagamentoId = formaDePagamentoId;
+    }
+}
---
+++ src/main/java/br/com/ylanbenoliel/pagamentos/model/Status.java
@@ -0,0 +1,7 @@
+package br.com.ylanbenoliel.pagamentos.model;
+
+public enum Status {
+    CRIADO,
+    CONFIRMADO,
+    CANCELADO
+}
